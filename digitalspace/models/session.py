36d1dc7ea833bc653256cec751d11562dc3acfdc
impossible <abdulmukhlismaulanayusuf@gmail.com>
2025-06-22 22:24:13 +0700

    Update Compute

--- digitalspace/models/session.py
+++ digitalspace/models/session.py
@@ -13,8 +13,20 @@ class Session(models.Model):
     description = fields.Text(string='Description')
 
     # Many2one
-    user_id = fields.Many2one('res.users', string='Instructor', domain="['!',('is_instructor','=',True),('teacher_level_id','!=',False)]")
+    user_id = fields.Many2one('res.users', string='Instructor', domain="['|',('is_instructor','=',True),('teacher_level_id','!=',False)]")
     course_id = fields.Many2one(comodel_name='digitalspace.course', string='Course', required=True)
 
     # Many2many
     partner_ids = fields.Many2many('res.partner', string='Attendees')
+
+
+    # Compute
+    taken_seats = fields.Float(compute='_compute_taken_seats', string='Taken Seats')
+    
+    @api.depends('partner_ids','number_of_seats')
+    def _compute_taken_seats(self):
+        for rec in self:
+            if rec.number_of_seats and rec.partner_ids:
+                rec.taken_seats = len(rec.partner_ids) / rec.number_of_seats * 100
+            else:
+                rec.taken_seats = 0
--- digitalspace/models/teacher_level.py
+++ digitalspace/models/teacher_level.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api
 
 class TeacherLevel(models.Model):
     _name = 'digitalspace.teacher_level'
+    _description = 'Teacher Level'
 
     name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
